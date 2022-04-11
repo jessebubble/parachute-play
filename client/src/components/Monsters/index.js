047eb277fb18ef08301f86949077b75d6964fbba
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-04-10 19:18:57 -0500

    removed drag constraint for better user responsiveness

--- client/src/components/Monsters/index.js
+++ client/src/components/Monsters/index.js
@@ -13,101 +13,37 @@ import MonsterNine from '../images/porange.svg';
 import MonsterTen from '../images/pred.svg';
 
 
-const Section = styled.section`
-    height: 100vh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    `;
-const Container = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    height: 100vh;
-    padding: 3rem calc((100vw-1300px) / 2)
-`;
+
 
 const Image = styled(motion.img)`
-    position: absolute;
-    width: 100%;
-    height: 100%;
-    max-width: 500px;
-    max-height: 250px;
+    max-width: 300px;
+    max-height: 150px;
     `; 
-const Column = styled.div`    
-    justify-content: center;
-    align-items: center;
-    padding: 7rem;
-    position: relative;
-    
-    ${Image}:nth-child(1) {
-      top: -0px;
-      left: 320px;
-    }
-    ${Image}:nth-child(2) {
-      top: 100px;
-      right: -40px;
-    }
-    ${Image}:nth-child(3) {
-      top: 80px;
-      left: -250px;
-    }
-    ${Image}:nth-child(4) {
-      bottom: 650px;
-      right: -520px;
-    }
-    ${Image}:nth-child(5) {
-      bottom: 1080px;
-      right: -525px;
-      max-width: 75px;
-    }
-    ${Image}:nth-child(6) {
-      bottom: 950px;
-      right: -650px;
-      max-width: 75px;
-    }
-    ${Image}:nth-child(7) {
-      bottom: 1270px;
-      right: 700px;
-      max-width: 75px;
-    }
-    ${Image}:nth-child(8) {
-      bottom: 900px;
-      right: 700px;
-      max-width: 75px;
-    }
-    ${Image}:nth-child(9) {
-      bottom: 1290px;
-      right: -640px;
-      max-width: 75px;
-    }
-    ${Image}:nth-child(10) {
-      bottom: 1055px;
-      right: 555px;
-      max-width: 75px;
-    }
+const Column = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    height:400px
+    align-content: space-between;
+    padding: 5rem;
 `;
 
 function Monsters () {
 
     return (
-        <Section>
-            <Container>
                 <Column>
                     <Image
                     src={MonsterOne}
                     alt='monster'
-                    whileTap={{ scale: 0.9 }}
+                    whileTap={{ scale: 0.8 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50 }}
-                    initial={{ opacity: 0, y: -100 }}
-                    animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
                     <Image
                     src={MonsterTwo}
                     alt='monster'
-                    whileTap={{ scale: 0.6 }}
+                    whileTap={{ scale: 0.8 }}
                     drag={true}
-                    dragConstraints={{ left: 50, right: 0, top: 0, bottom: 50 }}
                     initial={{ opacity: 0, x: 100 }}
                     animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
@@ -116,35 +52,31 @@ function Monsters () {
                     alt='monster'
                     whileTap={{ scale: 0.8 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 250, top: 0, bottom: 50 }}
-                    initial={{ opacity: 0, x: -100 }}
+                    initial={{ opacity: 0, x: 100 }}
                     animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
                     <Image
                     src={MonsterFour}
                     alt='monster'
-                    whileTap={{ scale: 0.9 }}
+                    whileTap={{ scale: 0.8 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
                     <Image
                     src={MonsterFive}
                     alt='monster'
                     whileTap={{ scale: 0.9 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
                     <Image
                     src={MonsterSix}
                     alt='monster'
                     whileTap={{ scale: 0.9 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                    initial={{ opacity: 0, y: 100 }}
+                    initial={{ opacity: 0, y: -100 }}
                     animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
                     />
                     <Image
@@ -152,8 +84,7 @@ function Monsters () {
                     alt='monster'
                     whileTap={{ scale: 0.9 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                    initial={{ opacity: 0, y: 100 }}
+                    initial={{ opacity: 0, y: -100 }}
                     animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
                     />
                     <Image
@@ -161,8 +92,7 @@ function Monsters () {
                     alt='monster'
                     whileTap={{ scale: 0.9 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                    initial={{ opacity: 0, y: 100 }}
+                    initial={{ opacity: 0, y: -100 }}
                     animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
                     />
                     <Image
@@ -170,7 +100,6 @@ function Monsters () {
                     alt='monster'
                     whileTap={{ scale: 0.9 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
                     initial={{ opacity: 0, y: 100 }}
                     animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
                     />
@@ -179,14 +108,10 @@ function Monsters () {
                     alt='monster'
                     whileTap={{ scale: 0.9 }}
                     drag={true}
-                    dragConstraints={{ left: 0, right: 0, top: 0, bottom: 0 }}
-                    initial={{ opacity: 0, y: 100 }}
-                    animate={{ opacity: 1, y: 0, transition: { duration: 1 } }}
+                    initial={{ opacity: 0, x: 100 }}
+                    animate={{ opacity: 1, x: 0, transition: { duration: 1 } }}
                     />
                 </Column>
-            </Container>
-        </Section>
     );
 }
-
 export default Monsters;
