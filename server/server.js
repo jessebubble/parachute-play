975960fe4f587434792da0471e121951f1aaa591
Jesse Hernandez <jessebubble@jesses-air.attlocal.net>
2022-04-13 14:34:19 -0500

    atlas serverjs update

--- server/server.js
+++ server/server.js
@@ -1,29 +1,30 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
-
-const {typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
+/* const startServer = async () => { */
   const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: authMiddleware,
+    context: authMiddleware
   });
-  await server.start();
+
+ /*  await server.start(); */
   server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+  /* console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`); */
+/* };
 
-startServer()
+startServer() */
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // app.use(
 //   '/graphql',
 //   graphqlExpress(req => ({
@@ -35,16 +36,14 @@ app.use(express.json());
 // );
 
 // Serve up static assets
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '../client/build')));
-// }
+if (process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, '../client/build')));
+ }
 
-// app.get('*', (req, res) => {
-//    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//  });
+app.get('*', (req, res) => {
+res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
